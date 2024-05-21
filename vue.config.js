550435edc3703b646028dadd8b85d3ca53ac610b
james m weeks <james.m.weeks@gmail.com>
2024-05-21 12:07:38 -0600

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  disableHostCheck: true,
-  port: 8080
+  devServer: {
+    allowedHosts: ['finer-finish-drywall-6003de34b6c9.herokuapp.com'],
+    port: process.env.PORT || 8080
+  }
 })
